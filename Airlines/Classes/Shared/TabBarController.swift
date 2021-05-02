b36efebd1f723bd66cb291231f3b6f601f024362
Gabriel Alves Moreira <g.alves.m.2008@gmail.com>
2021-05-02 13:33:59 -0300

    First commit

---
+++ Airlines/Classes/Shared/TabBarController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let firstTabViewController = ViewController()
+        firstTabViewController.tabBarItem = UITabBarItem(title: "Airlines", image: UIImage(systemName: "airplane"), tag: 0)
+        
+        let secondTabViewController = ViewController()
+        secondTabViewController.tabBarItem = UITabBarItem(title: "Passengers", image: UIImage(systemName: "person.fill"), tag: 1)
+        
+        viewControllers = [
+            UINavigationController(rootViewController: firstTabViewController),
+            UINavigationController(rootViewController: secondTabViewController)
+        ]
+    }
+}
