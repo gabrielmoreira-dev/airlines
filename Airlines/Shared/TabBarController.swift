2f1dfc4d0f84ae6a44fc5fbc57a9eea9583e2479
Gabriel Moreira <g.alves.m.2008@gmail.com>
2022-08-14 02:12:10 -0300

    Added localizable

--- Airlines/Shared/TabBarController.swift
+++ Airlines/Shared/TabBarController.swift
@@ -1,10 +1,12 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
+    private typealias Localizable = Strings.Navigation
+    
     private lazy var firstTabController: UINavigationController = {
         let navigationController = UINavigationController()
         navigationController.tabBarItem = UITabBarItem(
-            title: "Airlines",
+            title: Localizable.Airlines.title,
             image: UIImage(systemName: "airplane"),
             tag: 0
         )
@@ -18,7 +20,7 @@ final class TabBarController: UITabBarController {
     private lazy var secondTabController: UINavigationController = {
         let navigationController = UINavigationController()
         navigationController.tabBarItem = UITabBarItem(
-            title: "Passengers",
+            title: Localizable.Passengers.title,
             image: UIImage(systemName: "person.fill"),
             tag: 1
         )
